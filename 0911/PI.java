0a6289862c0c267f5eb71e604cb121d0713177f4
chan-repo <neolemon203@gmail.com>
2025-09-11 14:36:28 +0900

    Update PI.java

--- 0911/PI.java
+++ 0911/PI.java
@@ -1,1 +1,12 @@
+package chapter2;
 
+public class PI {
+    public static void main(String[] args) {
+        final double PI = 3.14;
+        double radius = 10.2;
+        double circleArea = radius * radius * PI;
+        
+        System.out.print("Radius = " + radius + ", ");
+        System.out.println("Area = " + circleArea);
+    }
+}
